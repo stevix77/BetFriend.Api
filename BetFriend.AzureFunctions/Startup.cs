a479cb6dd64c0c7d1123c14f973561ddaeb9132a
steve valentin <s.valentin77@hotmail.fr>
2021-10-15 15:08:33 +0200

    WIP

---
+++ BetFriend.AzureFunctions/Startup.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+
+[assembly: FunctionsStartup(typeof(BetFriend.AzureFunctions.Startup))]
+namespace BetFriend.AzureFunctions
+{
+    using BetFriend.Bet.Application.Abstractions;
+    using BetFriend.Bet.Infrastructure;
+    using BetFriend.UserAccess.Application.Abstractions;
+    using BetFriend.UserAccess.Infrastructure;
+    using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+
+    public class Startup : FunctionsStartup
+    {
+
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            //var configurationRoot = builder.Services.BuildServiceProvider().GetService<IConfiguration>();
+            ////builder.AddDependencies();
+            builder.Services.AddBetModule(default);
+            //builder.Services.AddScoped<IUserAccessModule, UserAccessModule>();
+        }
+    }
+}
