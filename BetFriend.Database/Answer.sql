ebe0f351ef54cf217c8a099f160701027cf29e11
stevix77 <s.valentin77@hotmail.fr>
2021-10-17 21:22:45 +0200

    rename files and add FK

--- BetFriend.Database/Answer.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[answer]
-(
-	[bet_id] UNIQUEIDENTIFIER NOT NULL,
-	[member_id] UNIQUEIDENTIFIER NOT NULL,
-	[is_accepted] BIT NOT NULL,
-	[date_answer] DATETIME2 NOT NULL
-	PRIMARY KEY (bet_id, member_id, is_accepted), 
-    CONSTRAINT [FK_Answer_Bet] FOREIGN KEY ([bet_id]) REFERENCES [bet]([bet_id])
-)
--- BetFriend.Database/dbo.Bet.sql
+++ BetFriend.Database/dbo.Bet.sql
@@ -1,4 +1,4 @@
-﻿CREATE TABLE [dbo].[bet]
+﻿CREATE TABLE [dbo].[Bet]
 (
 	[id] INT NOT NULL  IDENTITY (1, 1),
 	[bet_id] UNIQUEIDENTIFIER NOT NULL, 
@@ -7,5 +7,6 @@
 	[end_date] DATETIME2 NOT NULL,
 	[member_id] UNIQUEIDENTIFIER NOT NULL,
 	[creation_date] DATETIME2 NOT NULL 
-    PRIMARY KEY ([bet_id]) 
+    PRIMARY KEY ([bet_id]), 
+    CONSTRAINT [FK_Bet_Member] FOREIGN KEY ([member_id]) REFERENCES [Member]([Member_id]) 
 )
