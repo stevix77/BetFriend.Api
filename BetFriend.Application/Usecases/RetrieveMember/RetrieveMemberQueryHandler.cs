605a0d5f950387aa9f8bca827e7ae99f93c3e18e
steve valentin <s.valentin77@hotmail.fr>
2022-03-12 01:32:49 +0100

    ShouldNotRetrieveMemberWhenIdUnknown, ShouldRetrieveMemberWhenIdKnown

---
+++ BetFriend.Application/Usecases/RetrieveMember/RetrieveMemberQueryHandler.cs
@@ -0,0 +1,24 @@
+﻿using BetFriend.Bet.Application.Abstractions.Repository;
+using BetFriend.Bet.Application.Models;
+using BetFriend.Shared.Application.Abstractions.Query;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BetFriend.Bet.Application.Usecases.RetrieveMember
+{
+    public class RetrieveMemberQueryHandler : IQueryHandler<RetrieveMemberQuery, MemberDto>
+    {
+        private readonly IQueryMemberRepository _memberRepository;
+
+        public RetrieveMemberQueryHandler(IQueryMemberRepository queryMemberRepository)
+        {
+            _memberRepository = queryMemberRepository;
+        }
+
+        public async Task<MemberDto> Handle(RetrieveMemberQuery query, CancellationToken cancellationToken)
+        {
+            var memberDto = await _memberRepository.GetByIdAsync(query.MemberId);
+            return memberDto;
+        }
+    }
+}
