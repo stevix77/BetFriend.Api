27dc1052b2f6aa66aee2bc1065d2702193fcebb7
steve valentin <s.valentin77@hotmail.fr>
2022-03-12 01:32:49 +0100

    ShouldNotRetrieveMemberWhenIdUnknown, ShouldRetrieveMemberWhenIdKnown

---
+++ BetFriend.Application/Usecases/RetrieveMember/RetrieveMemberQuery.cs
@@ -0,0 +1,17 @@
+﻿namespace BetFriend.Bet.Application.Usecases.RetrieveMember
+{
+    using BetFriend.Bet.Application.Models;
+    using BetFriend.Shared.Application.Abstractions.Query;
+    using System;
+
+
+    public class RetrieveMemberQuery : IQuery<MemberDto>
+    {
+        public RetrieveMemberQuery(Guid memberId)
+        {
+            MemberId = memberId;
+        }
+
+        public Guid MemberId { get; }
+    }
+}
