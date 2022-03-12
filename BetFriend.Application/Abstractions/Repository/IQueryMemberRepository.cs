fb323369c8cc4f1ba767aab4c74117ea9ea97644
steve valentin <s.valentin77@hotmail.fr>
2022-03-12 01:32:49 +0100

    ShouldNotRetrieveMemberWhenIdUnknown, ShouldRetrieveMemberWhenIdKnown

---
+++ BetFriend.Application/Abstractions/Repository/IQueryMemberRepository.cs
@@ -0,0 +1,10 @@
+﻿using BetFriend.Bet.Application.Models;
+using System.Threading.Tasks;
+
+namespace BetFriend.Bet.Application.Abstractions.Repository
+{
+    public interface IQueryMemberRepository
+    {
+        Task<MemberDto> GetByIdAsync(System.Guid memberId);
+    }
+}
